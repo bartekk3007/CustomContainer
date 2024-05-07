b5b97010532826125a941d487f38c728fbf196c2
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-05-07 23:24:09 +0200

    Add files via upload

---
+++ CustomContainer/CustomContainer.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomContainer
+{
+    internal class CustomContainer<T> : IEnumerable<T>, IComparable<CustomContainer<T>>, IEquatable<CustomContainer<T>>
+    {
+        public List<T> children {  get; set; }
+        public CustomContainer(T arg)
+        {
+            children = new List<T>();
+            children.Add(arg);
+        }
+        public CustomContainer(IEnumerable<T> args)
+        {
+            children = new List<T>();
+            children.AddRange(args);
+        }
+        public void Add(T arg) 
+        { 
+            children.Add(arg);
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>)children).GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)children).GetEnumerator();
+        }
+        public int CompareTo(CustomContainer<T>? other)
+        {
+            if (other == null || other.children == null)
+            {
+                return 1;
+            }
+            if(children == null)
+            {
+                return -1;
+            }
+            if(children.Count != other.children.Count)
+            {
+                return children.Count - other.children.Count;
+            }
+            else
+            {
+                for(int i = 0; i < children.Count(); i++)
+                {
+                    if (other.children[i] == null)
+                    {
+                        return 1;
+                    }
+                    else if (children[i] == null)
+                    {
+                        return -1;
+                    }
+                    else if (children[i].GetHashCode() != other.children[i].GetHashCode())
+                    {
+                        return children[i].GetHashCode() - other.children[i].GetHashCode();
+                    }
+                }
+                return 0;
+            }
+        }
+        public bool Equals(CustomContainer<T>? other)
+        {
+            if(other == null || other.children == null)
+            { 
+                return false; 
+            }
+            else if(children == null)
+            { 
+                return false; 
+            }
+            else
+            {
+                return children.SequenceEqual(other.children);
+            }
+        }
+        public override string ToString()
+        {
+            string result = "";
+            foreach(var child in children)
+            {
+                result = child.ToString() + ", ";
+            }
+            return result;
+        }
+        public void Print()
+        {
+            foreach(var child in children)
+            {
+                Console.WriteLine(child.ToString());
+            }
+        }
+        public T this[int i]
+        {
+            get { return children[i]; }
+            set { children[i] = value; }
+        }
+    }
+}
---
+++ CustomContainer/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace CustomContainer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomContainer<int> custom1 = new CustomContainer<int>(1){};
+            custom1.Add(2);
+            custom1.Add(3);
+            custom1.Add(4);
+            custom1.Add(5);
+            custom1.Add(6);
+            custom1.Add(7);
+            custom1.Add(8);
+            custom1.Add(9);
+            custom1.Add(10);
+            custom1.Print();
+            Console.WriteLine(custom1[5]);
+            custom1[5] = 15;
+            Console.WriteLine(custom1[5]);
+            custom1.Print();
+        }
+    }
+}
